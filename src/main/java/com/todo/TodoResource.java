b4123b56fdd6c5c9d00e74d2683bb386099dba51
shrey-96 <tiwarishreyansh15@rediffmail.com>
2021-03-06 01:54:56 -0500

    Added delete todo api

--- src/main/java/com/todo/TodoResource.java
+++ src/main/java/com/todo/TodoResource.java
@@ -3,7 +3,9 @@ package com.todo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +23,15 @@ public class TodoResource {
 	@GetMapping("/users/{username}/todos")
 	public List<Todo> getAllTodos(@PathVariable String username) {
 		return todoService.findAll();
+	}
+	
+	@DeleteMapping("/users/{username}/todos/{todo_id}")
+	public ResponseEntity<Void> deleteTodoItem(@PathVariable String username, @PathVariable long todo_id) {
+		Todo todo = todoService.deleteById(todo_id);
+		
+		if(todo == null)
+			return ResponseEntity.notFound().build();
+		else
+			return ResponseEntity.ok(null);
 	}
 }
